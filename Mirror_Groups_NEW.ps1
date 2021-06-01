718f0cb1135963a427d413c2203720c27e9adfb3
Boston-of-Gilead <Boston-of-Gilead@users.noreply.github.com>
2021-06-01 07:52:52 -0400

    Add files via upload
    6/1/21 update

--- Mirror_Groups_NEW.ps1
+++ Mirror_Groups_NEW.ps1
@@ -12,8 +12,8 @@ Write-Host "Modules loaded"
 $Source = Read-Host -Prompt "Please enter the USERNAME of the user to be copied"
 $Dest = Read-Host -Prompt "Please enter the USERNAME of the user to receive group memberships"
 
-$badgroup = Get-ADGroup -Identity badgroupname
-$Recipients = "email1","email2","email3"
+$badgroup = Get-ADGroup -Identity excludegroup
+$Recipients = "email1","email2"
 $Sender = Read-Host -Prompt "Please enter YOUR EMAIL address"
 $SenderUser = $Sender.substring(0,$Sender.Length-10)
 
@@ -23,18 +23,13 @@ $MailUser = Get-ADUser $Dest -Properties DisplayName | Select -ExpandProperty Di
 $Signature = Get-ADUser -Identity $SenderUser -Properties DisplayName | Select -ExpandProperty DisplayName
 
 foreach($group in $CopyFromUser.memberof){ 
-    if (($group -ne $badgroup) -and ($group -notlike '*<exclude string>*')){
+    if (($group -ne $badgroup) -and ($group -notlike '*string*')){
     Add-ADGroupMember -Identity $group -Members $CopyToUser
     write-output $group
     }
-    elseif($group -eq $badgroup){Send-MailMessage -From $Sender -To $Recipients -Subject "MaximoUsers membership requested for $($MailUser)" -Body "Good day,
-BadGroup membership has been requested for $($MailUser). Do you approve?
+    elseif($group -eq $badgroup){Send-MailMessage -From $Sender -To $Recipients -Subject "ExcludeGroup membership requested for $($MailUser)" -Body "Good day,
+ExcludeGroup membership has been requested for $($MailUser). Do you approve?
 
 Kind Regards,
-$($Signature)" -SmtpServer <fqdn>}
+$($Signature)" -SmtpServer smtp.xxx.net} 
     }
-    #else{}
-
-#$CopyFromUser.MemberOf | Where{$CopyToUser.MemberOf -notcontains $_} | Where{$CopyFromUser.MemberOf -ne "badgroupname"} | Add-ADGroupMember -Members $CopyToUser | if ($CopyFromUser.MemberOf -eq "badgroupname"){
-#Send-MailMessage -From $Sender -To $Recipient -Subject "BadGroup membership requested for $($touser)" -Body "Good day,
-#BadGroup membership has been requested for $($touser) as well. Do you approve?" -SmtpServer <fqdn>}
